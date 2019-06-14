7ce9d23bc146f6f3b56207a0b5c4ceff2bb771a3
Luce Carter <luce@lucyscarter.co.uk>
2019-06-14 17:57:21 +0200

    Simple Shell Demo

---
+++ ShellDemoXamDay/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellDemoXamDay
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
